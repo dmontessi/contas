753128ad605e2d149abd4aae23283195cb660eb0
Daniel Matheus Montessi <montessi.daniel@gmail.com>
2024-01-03 15:24:27 -0400

    ajuste

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,6 +35,41 @@ class HomeController extends Controller
             ->whereMonth('vencimento', Carbon::today()->format('m'))
             ->sum('valor');
 
-        return view('home', compact('contas', 'pagos', 'abertos', 'total'));
+        $_contas = Conta::whereYear('vencimento', Carbon::today()->format('Y'))
+            ->whereMonth('vencimento', Carbon::today()->format('m'))
+            ->get();
+
+        $grafico = [];
+        $mesatual = 0;
+
+        foreach ($_contas as $_conta) {
+            $valor_atual = $grafico[$_conta->devedor->apelido]['valor'] ?? 0;
+            $grafico[$_conta->devedor->apelido]['valor'] = $valor_atual + $_conta->valor;
+
+            $mesatual = $mesatual + $_conta->valor;
+        }
+
+        $grafico2 = [];
+
+        $mesretrasado_label = ucfirst(Carbon::now()->subMonths(2)->locale('pt-BR')->isoFormat('MMMM[/]YYYY'));
+        $mesretrasado = Conta::whereYear('vencimento', Carbon::today()->subMonths(2)->format('Y'))
+            ->whereMonth('vencimento', Carbon::today()->subMonths(2)->format('m'))
+            ->sum('valor');
+
+        $grafico2[$mesretrasado_label]['valor'] = $mesretrasado;
+
+        $mespassado_label = ucfirst(Carbon::now()->subMonths(1)->locale('pt-BR')->isoFormat('MMMM[/]YYYY'));
+        $mespassado = Conta::whereYear('vencimento', Carbon::today()->subMonths(1)->format('Y'))
+            ->whereMonth('vencimento', Carbon::today()->subMonths(1)->format('m'))
+            ->sum('valor');
+
+        $grafico2[$mespassado_label]['valor'] = $mespassado;
+
+        $mesatual_label = ucfirst(Carbon::now()->locale('pt-BR')->isoFormat('MMMM[/]YYYY'));
+        $mesatual;
+
+        $grafico2[$mesatual_label]['valor'] = $mesatual;
+
+        return view('home', compact('contas', 'pagos', 'abertos', 'total', 'grafico', 'grafico2'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,118 +1,220 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
 
-    @php
-    $contador = $contas->count();
-    @endphp
+@php
+$contador = $contas->count();
+@endphp
 
-    <div class="d-flex flex-column my-4">
+<div class="d-flex flex-column my-4">
 
-        <div class="d-flex justify-content-center p-3 mx-5 my-2">
-            <div class="col-md-4">
-                <div class="card p-2 mx-2 text-bg-success">
-                    <div class="d-flex justify-content-between align-items-center px-2">
-                        <span class="fs-5 fw-bold">
-                            <i class="bi bi-check-circle me-1"></i>Pagos
-                        </span>
-                        <small class="fw-light">{{ ucfirst(\Carbon\Carbon::now()->locale('pt-BR')->isoFormat('MMMM [de] YYYY')) }}</small>
-                    </div>
-
-                    <hr class="my-1">
-
-                    <div class="d-flex justify-content-center">
-                        <span class="fs-3 fw-bold">{{ number_format($pagos, 2, ',', '.') }}</span>
-                    </div>
+    <div class="d-flex justify-content-center p-3 mx-5 my-2">
+        <div class="col-md-4">
+            <div class="card p-2 mx-2 text-bg-success">
+                <div class="d-flex justify-content-between align-items-center px-2">
+                    <span class="fs-5 fw-bold">
+                        <i class="bi bi-check-circle me-1"></i>Pagos
+                    </span>
+                    <small class="fw-light">{{ ucfirst(\Carbon\Carbon::now()->locale('pt-BR')->isoFormat('MMMM [de] YYYY')) }}</small>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card p-2 mx-2 text-bg-danger">
-                    <div class="d-flex justify-content-between align-items-center px-2">
-                        <span class="fs-5 fw-bold">
-                            <i class="bi bi-exclamation-triangle me-1"></i>Em aberto
-                        </span>
-                        <small class="fw-light">{{ ucfirst(\Carbon\Carbon::now()->locale('pt-BR')->isoFormat('MMMM [de] YYYY')) }}</small>
-                    </div>
 
-                    <hr class="my-1">
+                <hr class="my-1">
 
-                    <div class="d-flex justify-content-center">
-                        <span class="fs-3 fw-bold">{{ number_format($abertos, 2, ',', '.') }}</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card p-2 mx-2 text-bg-primary">
-                    <div class="d-flex justify-content-between align-items-center px-2">
-                        <span class="fs-5 fw-bold">
-                            <i class="bi bi-clipboard-data me-1"></i>Total
-                        </span>
-                        <small class="fw-light">{{ ucfirst(\Carbon\Carbon::now()->locale('pt-BR')->isoFormat('MMMM [de] YYYY')) }}</small>
-                    </div>
-
-                    <hr class="my-1">
-
-                    <div class="d-flex justify-content-center">
-                        <span class="fs-3 fw-bold">{{ number_format($total, 2, ',', '.') }}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        @if ($contador > 0)
-        <div class="d-flex justify-content-center mt-4">
-            <small class="d-inline-flex px-2 py-1 fw-semibold text-danger bg-danger-subtle border border-danger-subtle rounded-2">
-                <i class="bi bi-exclamation-triangle me-1"></i>
-                <span>Você possui contas vencendo hoje! 💸</span>
-            </small>
-        </div>
-
-        <div class="d-flex justify-content-center card p-3 mx-5 my-2">
-            <div class="table-responsive min-table-height">
-                <table class="table table-hover mb-0">
-                    <thead>
-                        <tr>
-                            <th class="text-center align-middle m-0 py-0 px-1" width="30%">Descrição</th>
-                            <th class="text-center align-middle m-0 py-0 px-1" width="40%">Fornecedor</th>
-                            <th class="text-center align-middle m-0 py-0 px-1" width="30%">Valor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($contas as $conta)
-                        <tr>
-                            <td class="text-center align-middle m-0 py-0 px-1" style="color:{{$conta->devedor->cor}}">
-                                <a href="{{ route('contas.edit', $conta->id) }}" class="list-group-item list-group-item-action">
-                                    {{ $conta->descricao }}
-                                </a>
-                            </td>
-                            <td class="text-center align-middle m-0 py-0 px-1" style="color:{{$conta->devedor->cor}}">
-                                <a href="{{ route('contas.edit', $conta->id) }}" class="list-group-item list-group-item-action">
-                                    {{ $conta->fornecedor->apelido }}
-                                </a>
-                            </td>
-                            <td class="text-center align-middle m-0 py-0 px-1" style="color:{{$conta->devedor->cor}}">
-                                <a href="{{ route('contas.edit', $conta->id) }}" class="list-group-item list-group-item-action">
-                                    {{ number_format($conta->valor, 2, ',', '.') }}
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                @else
-                <div class="d-flex justify-content-center mt-4">
-                    <small class="d-inline-flex mb-1 px-2 py-1 fw-semibold text-success bg-success-subtle border border-success-subtle rounded-2">
-                        <i class="bi bi-check2-circle me-1"></i>
-                        <span>Parabéns, você está em dia com suas contas! 🤑</span>
-                    </small>
-                </div>
                 <div class="d-flex justify-content-center">
-                    <small>Não há contas vencendo hoje</small>
+                    <span class="fs-3 fw-bold">{{ number_format($pagos, 2, ',', '.') }}</span>
                 </div>
-                @endif
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="card p-2 mx-2 text-bg-danger">
+                <div class="d-flex justify-content-between align-items-center px-2">
+                    <span class="fs-5 fw-bold">
+                        <i class="bi bi-exclamation-triangle me-1"></i>Em aberto
+                    </span>
+                    <small class="fw-light">{{ ucfirst(\Carbon\Carbon::now()->locale('pt-BR')->isoFormat('MMMM [de] YYYY')) }}</small>
+                </div>
 
+                <hr class="my-1">
+
+                <div class="d-flex justify-content-center">
+                    <span class="fs-3 fw-bold">{{ number_format($abertos, 2, ',', '.') }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-2 mx-2 text-bg-primary">
+                <div class="d-flex justify-content-between align-items-center px-2">
+                    <span class="fs-5 fw-bold">
+                        <i class="bi bi-clipboard-data me-1"></i>Total
+                    </span>
+                    <small class="fw-light">{{ ucfirst(\Carbon\Carbon::now()->locale('pt-BR')->isoFormat('MMMM [de] YYYY')) }}</small>
+                </div>
+
+                <hr class="my-1">
+
+                <div class="d-flex justify-content-center">
+                    <span class="fs-3 fw-bold">{{ number_format($total, 2, ',', '.') }}</span>
+                </div>
+            </div>
+        </div>
     </div>
-    @endsection
+
+    @if ($contador > 0)
+    <div class="d-flex justify-content-center mt-4">
+        <small class="d-inline-flex px-2 py-1 fw-semibold text-danger bg-danger-subtle border border-danger-subtle rounded-2">
+            <i class="bi bi-exclamation-triangle me-1"></i>
+            <span>Você possui contas vencendo hoje! 💸</span>
+        </small>
+    </div>
+
+    <div class="d-flex justify-content-center card p-3 mx-5 my-2">
+        <div class="table-responsive min-table-height">
+            <table class="table table-hover mb-0">
+                <thead>
+                    <tr>
+                        <th class="text-center align-middle m-0 py-0 px-1" width="30%">Descrição</th>
+                        <th class="text-center align-middle m-0 py-0 px-1" width="40%">Fornecedor</th>
+                        <th class="text-center align-middle m-0 py-0 px-1" width="30%">Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($contas as $conta)
+                    <tr>
+                        <td class="text-center align-middle m-0 py-0 px-1" style="color:{{$conta->devedor->cor}}">
+                            <a href="{{ route('contas.edit', $conta->id) }}" class="list-group-item list-group-item-action">
+                                {{ $conta->descricao }}
+                            </a>
+                        </td>
+                        <td class="text-center align-middle m-0 py-0 px-1" style="color:{{$conta->devedor->cor}}">
+                            <a href="{{ route('contas.edit', $conta->id) }}" class="list-group-item list-group-item-action">
+                                {{ $conta->fornecedor->apelido }}
+                            </a>
+                        </td>
+                        <td class="text-center align-middle m-0 py-0 px-1" style="color:{{$conta->devedor->cor}}">
+                            <a href="{{ route('contas.edit', $conta->id) }}" class="list-group-item list-group-item-action">
+                                {{ number_format($conta->valor, 2, ',', '.') }}
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    @else
+    <div class="d-flex justify-content-center mt-4">
+        <small class="d-inline-flex mb-1 px-2 py-1 fw-semibold text-success bg-success-subtle border border-success-subtle rounded-2">
+            <i class="bi bi-check2-circle me-1"></i>
+            <span>Parabéns, você está em dia com suas contas! 🤑</span>
+        </small>
+    </div>
+    <div class="d-flex justify-content-center">
+        <small>Não há contas vencendo hoje</small>
+    </div>
+    @endif
+
+    <div class="d-flex justify-content-center mt-4">
+        <div class="col-md-6">
+            <div class="card p-3 mx-1">
+                <canvas id="grafico1"></canvas>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card p-3 mx-1">
+                <canvas id="grafico2"></canvas>
+            </div>
+        </div>
+    </div>
+
+</div>
+<script type="module">
+    window.formatCurrency = function(value) {
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value);
+    }
+
+    var grafico1 = new Chart(document.getElementById('grafico1').getContext('2d'), {
+        type: 'bar',
+        data: {
+
+            labels: [
+                @foreach($grafico as $chave => $dados)
+                    '{{ $chave }}',
+                @endforeach
+            ],
+
+            datasets: [{
+                label: 'Valor por devedor',
+                data: [
+                    @foreach($grafico as $chave => $dados)
+                        {{ $dados['valor'] }},
+                    @endforeach
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        return formatCurrency(tooltipItem.yLabel);
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    ticks: {
+                        callback: function(value) {
+                            return formatCurrency(value);
+                        }
+                    }
+                }
+            }
+        }
+
+    });
+
+    var grafico2 = new Chart(document.getElementById('grafico2').getContext('2d'), {
+        type: 'bar',
+        data: {
+
+            labels: [
+                @foreach($grafico2 as $chave => $dados)
+                    '{{ $chave }}',
+                @endforeach
+            ],
+
+            datasets: [{
+                label: 'Valor por mês',
+                data: [
+                    @foreach($grafico2 as $chave => $dados)
+                        {{ $dados['valor'] }},
+                    @endforeach
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        return formatCurrency(tooltipItem.yLabel);
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    ticks: {
+                        callback: function(value) {
+                            return formatCurrency(value);
+                        }
+                    }
+                }
+            }
+        }
+
+    });
+</script>
+@endsection
